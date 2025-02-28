26798c1312a983663a0f5328bf522c857c3bd87f
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2025-02-28 18:05:59 +0100

    auth clerk part v1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-//import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { Dashboard } from "./pages/dashboard";
+import { Auth } from "./pages/auth";
+
 
 export default function App() {
   return (
       <Router>
         <div className="app-container">
           <Routes>
-            <Route path="/" element={<h1>Dashboard</h1>}/>
-            <Route path="/auth" element={<h1>Sign In</h1>}/>
+            <Route path="/" element={<Dashboard />}/>
+            <Route path="/auth" element={<Auth />}/>
           </Routes>
         </div>
       </Router>
