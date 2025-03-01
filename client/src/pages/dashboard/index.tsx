f868e94c94052b50d544088f1c2162dcf70b9183
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2025-03-01 18:39:20 +0100

    record form fe v1

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,10 +1,14 @@
 import { useUser } from "@clerk/clerk-react";
+import { FinancialRecordForm } from "./financial-record-form";
+import { FinancialRecordList } from "./finansial-record-list";
 
 export const Dashboard = () => {
   const { user } = useUser();
   return (
     <div className="dashboard-container">
       <h1>Hello {user?.firstName}! Here are your finances!</h1>
+      <FinancialRecordForm />
+      <FinancialRecordList />
     </div>
   );
 };
