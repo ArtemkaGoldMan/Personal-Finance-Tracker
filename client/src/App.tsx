d91a27c5b7470c6c8446f49bc9db81f3b93a0e39
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2025-03-04 18:48:37 +0100

    context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,25 @@
 import "./App.css";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Dashboard } from "./pages/dashboard";
 import { Auth } from "./pages/auth";
-
+import { FinancialRecordsProvider } from "./contexts/financial-record-context";
 
 export default function App() {
   return (
-      <Router>
-        <div className="app-container">
-          <Routes>
-            <Route path="/" element={<Dashboard />}/>
-            <Route path="/auth" element={<Auth />}/>
-          </Routes>
-        </div>
-      </Router>
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <FinancialRecordsProvider>
+                <Dashboard />
+              </FinancialRecordsProvider>
+            }
+          />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
--- client/src/pages/dashboard/financial-record-form.tsx
+++ client/src/pages/dashboard/financial-record-form.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { useUser } from "@clerk/clerk-react";
+import { useFinancialRecords } from "../../contexts/financial-record-context";
 
 export const FinancialRecordForm = () => {
   const [description, setDescription] = useState<string>("");
   const [amount, setAmount] = useState<string>("");
   const [category, setCategory] = useState<string>("");
   const [paymentMethod, setPaymentMethod] = useState<string>("");
+  const { addRecord } = useFinancialRecords();
 
   const { user } = useUser();
 
@@ -13,7 +15,7 @@ export const FinancialRecordForm = () => {
     event.preventDefault();
 
     const newRecord = {
-      userId: user?.id,
+      userId: user?.id ?? "",
       date: new Date(),
       description: description,
       amount: parseFloat(amount),
@@ -21,7 +23,7 @@ export const FinancialRecordForm = () => {
       paymentMethod: paymentMethod,
     };
 
-    //addRecord(newRocord);
+    addRecord(newRecord);
     setDescription("");
     setAmount("");
     setCategory("");
@@ -30,7 +32,7 @@ export const FinancialRecordForm = () => {
 
   return (
     <div className="form-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-field">
           <label>Description:</label>
           <input
